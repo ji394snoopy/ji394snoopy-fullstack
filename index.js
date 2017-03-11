7c5529bbe46f19ca6cd87f56944fae68db2dfbe6
ji394snoopy <eric60513@gmail.com>
2017-03-11 15:04:24 +0800

    update simple node js file

--- index.js
+++ index.js
@@ -1,32 +1,22 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var path = require('path');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public/resume')));
 
 // views is directory for all template files
 //app.set('views', __dirname + '/views');
 //app.set('view engine', 'ejs');
 
-var localPath = "/resume/resume.html"
-
 app.get('/', function(request, response) {
-  //response.render('pages/index');
-  fs.readFile(localPath, function(err, contents) {
-		if(!err) {
-			response.setHeader("Content-Length", contents.length);
-			response.statusCode = 200;
-			response.end(contents);
-		} else {
-			response.writeHead(500);
-			response.end();
-		}
-});
+    //response.render('pages/index');
+    console.log('get /');
+    response.redirect('resume.html');
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
-
-
