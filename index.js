15f36c48762a6046789582ff2c33b333184957b1
ji394snoopy <eric60513@gmail.com>
2017-03-11 11:46:44 +0800

    change node for test

--- index.js
+++ index.js
@@ -6,11 +6,23 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
+
+var localPath = "/resume/resume.html"
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  //response.render('pages/index');
+  fs.readFile(localPath, function(err, contents) {
+		if(!err) {
+			response.setHeader("Content-Length", contents.length);
+			response.statusCode = 200;
+			response.end(contents);
+		} else {
+			response.writeHead(500);
+			response.end();
+		}
+});
 });
 
 app.listen(app.get('port'), function() {
