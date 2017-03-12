9690bf264857721c7de0ef511a3db98ef2454f61
ji394snoopy <eric60513@gmail.com>
2017-03-12 13:57:44 +0800

    update simple node js file v1.0

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var path = require('path');
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use('/css',express.static(path.join(__dirname,'public/resume/css')));
+app.use('/modules',express.static(path.join(__dirname,'public/resume/node_modules')));
+app.use('/js',express.static(path.join(__dirname,'public/resume/script')));
+
 app.use(express.static(path.join(__dirname, 'public/resume')));
 
 // views is directory for all template files
@@ -13,8 +17,40 @@ app.use(express.static(path.join(__dirname, 'public/resume')));
 
 app.get('/', function(request, response) {
     //response.render('pages/index');
-    console.log('get /');
-    response.redirect('resume.html');
+    var filePath = request.url;
+    console.log(request.url);
+    if (filePath == '/')
+        filePath = '/resume.html';
+
+    filePath = __dirname + '/public/resume' + filePath;
+    console.log(filePath);
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+
+    console.log(extname);
+
+    fs.exists(filePath, function(exists) {
+        if (exists) {
+            fs.readFile(filePath, function(error, content) {
+                if (error) {
+                    response.writeHead(500);
+                    response.end();
+                } else {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                }
+            });
+        }
+    });
 });
 
 app.listen(app.get('port'), function() {
