037fc14ab4a709db50d937d5b2e9f5b644cbd2ff
ji394snoopy <eric60513@gmail.com>
2017-03-12 14:26:34 +0800

    update simple change v1.1

--- index.js
+++ index.js
@@ -9,48 +9,47 @@ app.set('port', (process.env.PORT || 5000));
 // app.use('/modules',express.static(path.join(__dirname,'public/resume/node_modules')));
 // app.use('/js',express.static(path.join(__dirname,'public/resume/script')));
 
-app.use(express.static(path.join(__dirname, 'public/resume')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-    response.render('pages/index');
-    // var filePath = request.url;
-    // console.log(request.url);
-    // if (filePath == '/')
-    //     filePath = '/resume.html';
+    // response.render('pages/index');
+    var filePath = request.url;
+    if (filePath == '/')
+        filePath = '/resume.html';
 
-    // filePath = __dirname + '/public/resume' + filePath;
-    // console.log(filePath);
-    // var extname = path.extname(filePath);
-    // var contentType = 'text/html';
+    filePath = __dirname + '/public/resume' + filePath;
 
-    // switch (extname) {
-    //     case '.js':
-    //         contentType = 'text/javascript';
-    //         break;
-    //     case '.css':
-    //         contentType = 'text/css';
-    //         break;
-    // }
+    console.log(filePath);
 
-    // console.log(extname);
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
 
-    // fs.exists(filePath, function(exists) {
-    //     if (exists) {
-    //         fs.readFile(filePath, function(error, content) {
-    //             if (error) {
-    //                 response.writeHead(500);
-    //                 response.end();
-    //             } else {
-    //                 response.writeHead(200, { 'Content-Type': contentType });
-    //                 response.end(content, 'utf-8');
-    //             }
-    //         });
-    //     }
-    // });
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+
+    fs.exists(filePath, function(exists) {
+        if (exists) {
+            fs.readFile(filePath, function(error, content) {
+                if (error) {
+                    response.writeHead(500);
+                    response.end();
+                } else {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                }
+            });
+        }
+    });
 });
 
 app.listen(app.get('port'), function() {
