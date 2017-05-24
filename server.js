7b63388d49f215b6d27c0ebf76d8225d98f1635d
ji394snoopy <eric60513@gmail.com>
2017-05-24 23:37:32 +0800

    change all things into single vue components and replace public/not resume/..

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 var express = require('express');
-var app = express();
 var fs = require('fs');
 var path = require('path');
+var serveStatic = require('serve-static');
+var history = require('connect-history-api-fallback');
+
+var app = express();
+
 
 app.set('port', (process.env.PORT || 5000));
 
 // app.use('/css',express.static(path.join(__dirname,'public/resume/css')));
 // app.use('/modules',express.static(path.join(__dirname,'public/resume/node_modules')));
 // app.use('/js',express.static(path.join(__dirname,'public/resume/script')));
-
+app.use(history());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(serveStatic(__dirname));
 
 // views is directory for all template files
 // app.set('views', __dirname + '/views');
@@ -17,25 +22,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(request, response) {
     // response.render('pages/index');
-    var filePath = request.url;
-    if (filePath == '/')
-        filePath = '/resume.html';
+    // var filePath = request.url;
+    // if (filePath === '/')
+    //     filePath = '/resume.html';
 
-    filePath = __dirname + '/public/not resume' + filePath;
+    var filePath = __dirname + '/index.html';
 
-    console.log(filePath);
+    // console.log(filePath);
 
-    var extname = path.extname(filePath);
-    var contentType = 'text/html';
+    // var extname = path.extname(filePath);
+    // var contentType = 'text/html';
 
-    switch (extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-    }
+    // switch (extname) {
+    //     case '.js':
+    //         contentType = 'text/javascript';
+    //         break;
+    //     case '.css':
+    //         contentType = 'text/css';
+    //         break;
+    // }
 
     fs.exists(filePath, function(exists) {
         if (exists) {
@@ -44,7 +49,7 @@ app.get('/', function(request, response) {
                     response.writeHead(500);
                     response.end();
                 } else {
-                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.writeHead(200, { 'Content-Type': 'text/html' });
                     response.end(content, 'utf-8');
                 }
             });
