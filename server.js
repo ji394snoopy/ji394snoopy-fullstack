1c51ec22f996a12cd8d89660a39dcfa996a39e16
ji394snoopy <eric60513@gmail.com>
2017-09-17 15:10:29 +0800

    del screen shot

--- server.js
+++ server.js
@@ -1,62 +1,63 @@
-var express = require('express');
-var fs = require('fs');
-var path = require('path');
-var serveStatic = require('serve-static');
-var history = require('connect-history-api-fallback');
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const serveStatic = require('serve-static')
+const history = require('connect-history-api-fallback')
 
-var app = express();
+const app = express()
 
+app.set('port', (process.env.PORT || 5000))
 
-app.set('port', (process.env.PORT || 5000));
-
-// app.use('/css',express.static(path.join(__dirname,'public/resume/css')));
-// app.use('/modules',express.static(path.join(__dirname,'public/resume/node_modules')));
-// app.use('/js',express.static(path.join(__dirname,'public/resume/script')));
-app.use(history());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(serveStatic(__dirname));
+// app.use('/css',express.static(path.join(__dirname,'public/resume/css')))
+// app.use('/modules',express.static(path.join(__dirname,'public/resume/node_modules')))
+// app.use('/js',express.static(path.join(__dirname,'public/resume/script')))
+app.use(history())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(serveStatic(__dirname))
 
 // views is directory for all template files
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
+// app.set('views', __dirname + '/views')
+// app.set('view engine', 'ejs')
 
-app.get('/', function(request, response) {
-    // response.render('pages/index');
-    // var filePath = request.url;
-    // if (filePath === '/')
-    //     filePath = '/resume.html';
+app.get('/', function (request, response) {
+  // response.render('pages/index')
+  // var filePath = request.url
+  // if (filePath === '/')
+  //     filePath = '/resume.html'
 
-    var filePath = __dirname + '/index.html';
+  var filePath = __dirname + '/index.html'
 
-    // console.log(filePath);
+  // console.log(filePath)
 
-    // var extname = path.extname(filePath);
-    // var contentType = 'text/html';
+  // var extname = path.extname(filePath)
+  // var contentType = 'text/html'
 
-    // switch (extname) {
-    //     case '.js':
-    //         contentType = 'text/javascript';
-    //         break;
-    //     case '.css':
-    //         contentType = 'text/css';
-    //         break;
-    // }
+  // switch (extname) {
+  //     case '.js':
+  //         contentType = 'text/javascript'
+  //         break
+  //     case '.css':
+  //         contentType = 'text/css'
+  //         break
+  // }
 
-    fs.exists(filePath, function(exists) {
-        if (exists) {
-            fs.readFile(filePath, function(error, content) {
-                if (error) {
-                    response.writeHead(500);
-                    response.end();
-                } else {
-                    response.writeHead(200, { 'Content-Type': 'text/html' });
-                    response.end(content, 'utf-8');
-                }
-            });
+  fs.exists(filePath, function (exists) {
+    if (exists) {
+      fs.readFile(filePath, function (error, content) {
+        if (error) {
+          response.writeHead(500)
+          response.end()
+        } else {
+          response.writeHead(200, {
+            'Content-Type': 'text/html'
+          })
+          response.end(content, 'utf-8')
         }
-    });
-});
+      })
+    }
+  })
+})
 
-app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
-});
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'))
+})
